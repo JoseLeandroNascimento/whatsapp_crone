f28b39e392b681458c977f64ed6957689278ae94
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-05-27 07:17:58 -0500

    fix: ajuste de cores

--- app/src/main/java/com/example/whatsapp_crone/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/whatsapp_crone/screens/homeScreen/HomeScreen.kt
@@ -1,8 +1,8 @@
 package com.example.whatsapp_crone.screens.homeScreen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,7 +10,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
@@ -22,29 +24,39 @@ import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.whatsapp_crone.R
 import com.example.whatsapp_crone.ui.theme.Green
-import com.example.whatsapp_crone.ui.theme.White
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+
+    var showMenu by remember { mutableStateOf(false) }
+
 
     Scaffold(
         modifier = modifier,
@@ -58,17 +70,54 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     )
                 },
                 actions = {
-                    IconButton(onClick = {}) {
+                    IconButton(
+                        onClick = {}
+                    ) {
                         Icon(
                             Icons.Filled.ShoppingCart,
                             contentDescription = ""
                         )
                     }
-                    IconButton(onClick = {}) {
-                        Icon(
-                            Icons.Filled.MoreVert,
-                            contentDescription = ""
-                        )
+                    Box(
+                        modifier = Modifier.wrapContentSize(align = Alignment.TopStart)
+                    ) {
+                        IconButton(
+                            onClick = {
+                                showMenu = !showMenu
+                            }
+                        ) {
+                            Icon(
+                                Icons.Filled.MoreVert,
+                                contentDescription = ""
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = showMenu,
+                            containerColor = MaterialTheme.colorScheme.background,
+                            onDismissRequest = {
+                                showMenu = false
+                            }) {
+                            DropdownMenuItem(text = { Text(text = "Novo grupo") }, onClick = {})
+                            DropdownMenuItem(
+                                text = { Text(text = "Nova Comunidade") },
+                                onClick = {})
+                            DropdownMenuItem(
+                                text = { Text(text = "Nova Trasmissão") },
+                                onClick = {})
+                            DropdownMenuItem(
+                                text = { Text(text = "Dispositivos conectados") },
+                                onClick = {})
+                            DropdownMenuItem(text = { Text(text = "Favoritos") }, onClick = {})
+                            DropdownMenuItem(
+                                text = { Text(text = "Encontrar empresas") },
+                                onClick = {})
+                            DropdownMenuItem(text = { Text(text = "Pagamentos") }, onClick = {})
+                            DropdownMenuItem(
+                                text = { Text(text = "Marcar tudo como lido") },
+                                onClick = {})
+                            DropdownMenuItem(text = { Text(text = "Configurações") }, onClick = {})
+
+                        }
                     }
                 },
             )
@@ -85,12 +134,17 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             }
         },
         bottomBar = {
-            BottomAppBar {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.background
+            ) {
                 NavigationBarItem(
                     onClick = {},
                     selected = true,
+                    colors = NavigationBarItemDefaults.colors(
+                        indicatorColor = Green.copy(alpha = 0.2f)
+                    ),
                     label = {
-                        Text(text = "Conversas")
+                        Text(text = "Conversas", style = MaterialTheme.typography.labelSmall)
                     },
                     icon = {
                         Icon(
@@ -102,7 +156,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     onClick = {},
                     selected = false,
                     label = {
-                        Text(text = "Atualizações")
+                        Text(text = "Atualizações", style = MaterialTheme.typography.labelSmall)
                     },
                     icon = {
                         Icon(
@@ -115,7 +169,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     onClick = {},
                     selected = false,
                     label = {
-                        Text(text = "Comunidades")
+                        Text(text = "Comunidades", style = MaterialTheme.typography.labelSmall)
                     },
                     icon = {
                         Icon(
@@ -128,7 +182,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     onClick = {},
                     selected = false,
                     label = {
-                        Text(text = "Ligações")
+                        Text(text = "Ligações", style = MaterialTheme.typography.labelSmall)
                     },
                     icon = {
                         Icon(
@@ -162,7 +216,7 @@ fun MessageContact(modifier: Modifier = Modifier) {
     Card(
         modifier = modifier.fillMaxWidth(),
         colors = CardDefaults.cardColors(
-            containerColor = White
+            containerColor = MaterialTheme.colorScheme.background
         ),
         shape = RoundedCornerShape(0.dp)
     ) {
@@ -170,7 +224,9 @@ fun MessageContact(modifier: Modifier = Modifier) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
-                modifier = Modifier.size(60.dp),
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(shape = CircleShape),
                 painter = painterResource(R.drawable.ic_launcher_background),
                 contentDescription = ""
             )
@@ -182,7 +238,7 @@ fun MessageContact(modifier: Modifier = Modifier) {
                     ),
                 verticalArrangement = Arrangement.spacedBy(4.dp)
             ) {
-                Text(text = "David", style = MaterialTheme.typography.titleSmall)
+                Text(text = "David", style = MaterialTheme.typography.titleSmall, fontSize = 16.sp)
                 Text(text = "Ola, bom dia!", style = MaterialTheme.typography.bodySmall)
             }
         }
--- app/src/main/java/com/example/whatsapp_crone/ui/theme/Color.kt
+++ app/src/main/java/com/example/whatsapp_crone/ui/theme/Color.kt
@@ -10,5 +10,5 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val Green = Color(0xFF128c7e)
+val Green = Color(0xFF1DAA61)
 val White = Color(0xFFFFFFFF)
